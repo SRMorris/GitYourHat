f1937c6f9632d522ad1bd98dded593dd0da2c36a
Kristian Carter <kristiancarter@Kristians-MacBook-Air.local>
2015-12-01 09:43:40 -0600

    yea

---
+++ GetYourHat/MainCharacter.swift
@@ -0,0 +1,13 @@
+//
+//  MainCharacter.swift
+//  GetYourHat
+//
+//  Created by Kristian Carter on 12/1/15.
+//  Copyright © 2015 LNG. All rights reserved.
+//
+
+import UIKit
+
+class MainCharacter: NSObject {
+
+}
